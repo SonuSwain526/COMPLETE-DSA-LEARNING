25201ac78ad21f29d3ab536d7fb7fd6cc155c336
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-02-08 11:02:02 +0530

    arr of perm

--- things-before-concept/Arrays/src/PermArr.java
+++ things-before-concept/Arrays/src/PermArr.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class PermArr {
+    public static void main(String[] args) {
+        int[] arr = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(perm(arr)));
+    }
+
+    static int[] perm(int[] arr){
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
 }
