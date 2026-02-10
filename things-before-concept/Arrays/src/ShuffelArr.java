c5858ef0863e52baf725c82a98d8ab33c06c98a7
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-02-10 23:00:28 +0530

    array shuffeled

--- things-before-concept/Arrays/src/ShuffelArr.java
+++ things-before-concept/Arrays/src/ShuffelArr.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class ShuffelArr {
+    public static void main(String[] args) {
+        int[] nums= {1,2,3,2,3,4};
+        int n = 3;
+        int[] x = new int[n*2];
+        int y =0;
+        for(int i=0; i<n; i++){
+            x[y++] = nums[i];
+            x[y++] = nums[i+n];
+        }
+        System.out.println(Arrays.toString(x));
+
+    }
 }
