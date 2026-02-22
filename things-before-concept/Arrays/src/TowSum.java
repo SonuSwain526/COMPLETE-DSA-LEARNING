9d665c8cb1a2f99b299afe92db9008ff95bd2f52
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-02-22 23:42:02 +0530

    2sum

--- things-before-concept/Arrays/src/TowSum.java
+++ things-before-concept/Arrays/src/TowSum.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class TowSum {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,4};
+        int[] x = new int[2];
+        int target = 2;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i == j){
+                    continue;
+                }
+                if(nums[i] + nums[j] == target){
+                    x[0] = j;
+                    x[1] = i;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(x));
+    }
 }
