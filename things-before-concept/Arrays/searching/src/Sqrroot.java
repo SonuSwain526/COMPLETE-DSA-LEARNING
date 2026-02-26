a2668271895aca984a2b68be621f77cd56bf639d
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-02-26 22:03:05 +0530

    squrrt applying bs

--- things-before-concept/Arrays/searching/src/Sqrroot.java
+++ things-before-concept/Arrays/searching/src/Sqrroot.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Sqrroot {
+    public static void main(String[] args) {
+        int n = 300000;
+        int start = 0;
+        int end = n;
+        int ans=0;
+        while(start<end){
+            int mid = (start+end)/2;
+            if(n == ((long) mid *mid)){
+                System.out.println(mid);
+                return;
+            }
+           else if(((long) mid * mid) < n){
+                start = start + 1;
+                ans = mid;
+
+            }
+            else if(((long) mid * mid) > n){
+                end = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
 }
