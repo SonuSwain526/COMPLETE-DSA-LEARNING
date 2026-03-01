ea66703fa78bd5cbaec33901fa3e98748a3f4d76
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-01 23:53:27 +0530

    satday contest q3

--- things-before-concept/contest-questionss/src/Q2.java
+++ things-before-concept/contest-questionss/src/Q2.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Q2 {
+    public static void main(String[] args) {
+        int[] arr = {2,1,3};
+        int[] y = x(arr);
+        for (int j : y) {
+            System.out.print(j + " ");
+        }
+    }
+    public static int[] x(int[] a){
+        int[] y = new int[a.length];
+        int i = 0, j = a.length-1;
+        while (i<=j){
+            y[i] = a[j];
+            y[j] = a[i];
+            i++;
+            j--;
+        }
+        return y;
+    }
 }
