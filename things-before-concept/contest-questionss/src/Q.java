56eea09505296273f47d94fc9d308ae7d0a0267a
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-01 23:52:49 +0530

    satday contest q2

--- things-before-concept/contest-questionss/src/Q.java
+++ things-before-concept/contest-questionss/src/Q.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Q {
+    public static void main(String[] args) {
+        int[] x = {2, 3, 2, 2, 3};
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : x) {
+            set.add(num);
+        }
+        System.out.println(set.size());
+    }
 }
