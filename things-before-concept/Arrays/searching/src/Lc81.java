8004007b001468abeaa3670a6f065dee13e227f4
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-01 23:46:54 +0530

    0ms and 100% beats

--- things-before-concept/Arrays/searching/src/Lc81.java
+++ things-before-concept/Arrays/searching/src/Lc81.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Lc81 {
+        public static void main(String[] args) {
+            int[] arr = {1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1};
+            int target = 2;
+            int pivot = findIx(arr, target);
+            System.out.println(pivot);
+            System.out.println(bs(arr,target,0, pivot));
+            System.out.println(bs(arr,target,pivot+1, arr.length-1));
+//        if()
+        }
+        public static int findIx(int[] x, int t){
+            int s = 0, e = x.length-1;
+            while (s<=e){
+                int mid = (s+e)/2;
+                if(x[mid] > x[mid+1]){
+                    return mid;
+                }
+                if (x[s] == x[e] && x[mid] == x[e]){
+                    s++;
+                    e--;
+                }
+                else if (x[mid] >= x[s]) {
+                    s = mid+1;
+                }
+                else
+                    e = mid - 1;
+            }
+            return (s);
+        }
+        public static int bs(int[] x, int target, int s, int e){
+            while (s<=e){
+                int mid = (s+e)/2;
+                if(x[mid] == target){
+                    return mid;
+                } else if (x[mid] < target) {
+                    s = mid+1;
+                }
+                else
+                    e = mid - 1;
+            }
+            return -1;
+        }
 }
