030fbe97ce4485fdd2121e0c78978ba05edd208d
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-03 10:58:48 +0530

    lecture question

--- things-before-concept/lecture-questions/src/Q1bs.java
+++ things-before-concept/lecture-questions/src/Q1bs.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Q1bs {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        System.out.println(x(arr, 8));
+    }
+    public static int x(int[] a, int target){
+        int s = 0, e = a.length-1;
+        while(s <= e){
+            int mid = (s+e) / 2;
+            if(a[mid] == target){
+                return mid;
+            } else if (a[mid] < target) {
+                s = mid+1;
+            }else
+                e = mid-1;
+        }
+        return s-1;
+    }
 }
