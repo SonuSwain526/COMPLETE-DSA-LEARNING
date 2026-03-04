52dc2f01003b9320e1a2c75c56e495307e2c709d
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-04 23:09:19 +0530

    2nd parttern of bs, bloomlight

--- things-before-concept/atoz-question-submission/binary-search/src/Lc1482med.java
+++ things-before-concept/atoz-question-submission/binary-search/src/Lc1482med.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc1482med {
+    public static void main(String[] args) {
+        int[] b = {7,7,7,7,12,7,7};
+        System.out.println(minDays(b, 2, 3));
+    }
+    public static int minDays(int[] bloomDay, int m, int k) {
+        boolean[] days = new boolean[bloomDay.length];
+
+        int s = Integer.MAX_VALUE, e = 0;
+        for(int x: bloomDay){
+            s = Math.min(s, x);
+            e = Math.max(e, x);
+        }
+        int midD = -1;
+        while (s<e){
+            Arrays.fill(days, false);
+            int mid = (s+e)/2;
+            for (int i = 0; i < bloomDay.length; i++) {
+                if(bloomDay[i] <= mid){
+                    days[i] = true;
+                }
+            }
+            int x = 0;int ex = 0;
+            for (boolean day : days) {
+                if (day) {
+                    x++;
+                }
+                if (x == k) {
+                    ex++;
+                    x = 0;
+                } else
+                    x = 0;
+            }
+            if(x < m){
+                s = mid+1;
+            } else{
+                e = mid;
+            }
+
+        }
+        return e;
+
+    }
 }
