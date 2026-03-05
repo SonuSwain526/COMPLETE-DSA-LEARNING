342fa979bc4aafbb1a252d7c9542d8dfead05fe3
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-06 00:00:07 +0530

    hard 2d arr

--- things-before-concept/Arrays/searching/src/Lc315hard.java
+++ things-before-concept/Arrays/searching/src/Lc315hard.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Lc315hard {
+    public static void main(String[] args) {
+        int[] x = {5,1,2,6,1};
+        System.out.println(countSmaller(x));
+    }
+    public static List<Integer> countSmaller(int[] nums) {
+        List<Integer> x = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        return x;
+    }
 }
