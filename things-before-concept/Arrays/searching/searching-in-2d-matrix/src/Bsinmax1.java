09f1b0828a9998c5d00434cf90dc76ac340f2900
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-05 23:58:14 +0530

    bs in log(nm) tiemc

--- things-before-concept/Arrays/searching/searching-in-2d-matrix/src/Bsinmax1.java
+++ things-before-concept/Arrays/searching/searching-in-2d-matrix/src/Bsinmax1.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Bsinmax1 {
+    public static void main(String[] args) {
+        int[][] x = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}
+        };
+        System.out.println(Arrays.toString(bs(x, 9)));
+    }
+    public static int[] bs(int[][] x, int t){
+        int clm = x[0].length-1;
+        int row = 0;
+        while (clm >= 0 && row < x.length){
+            if(x[row][clm] == t){
+                return new int[]{row, clm};
+            } else if (x[row][clm] < t) {
+                row++;
+            }else clm--;
+        }
+        return new int[]{-1,-1};
+    }
 }
