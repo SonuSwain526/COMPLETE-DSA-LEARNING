161613f3d6198635f0fc3abaccf41606ad2b5b28
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-06 20:50:56 +0530

    started sorting, sellection sort

--- things-before-concept/Arrays/sorting/src/SelectionSort.java
+++ things-before-concept/Arrays/sorting/src/SelectionSort.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] x = {1,32,-10,0,100000};
+        int[] y = {};
+        SelectSort(x);
+    }
+    static void SelectSort(int[] x){
+        for (int i = 0; i < x.length - 1; i++) {
+            int larelem = largElem(x,i);
+            int temp = x[i];
+            x[i] = x[larelem];
+            x[larelem] = temp;
+        }
+        System.out.println(Arrays.toString(x));
+    }
+    static int largElem(int[] x, int i){
+        int curLarge = i;
+        for (int k = i; k < x.length; k++) {
+            if(x[curLarge] < x[k]){
+                curLarge = k;
+            }
+
+        }
+        return curLarge;
+    }
 }
