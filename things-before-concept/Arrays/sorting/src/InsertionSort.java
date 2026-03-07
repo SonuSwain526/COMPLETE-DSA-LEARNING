524ef5bfc134cd120a2ed9b49ae0da0f8b549a79
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-07 23:19:42 +0530

    Insertion sort

--- things-before-concept/Arrays/sorting/src/InsertionSort.java
+++ things-before-concept/Arrays/sorting/src/InsertionSort.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] x = {1,32,-10,0,100000};
+        int[] y = {};
+        inserSort(x);
+    }
+    static void inserSort(int[] x){
+        for (int i = 0; i < x.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(x[j - 1] > x[j]){
+                    int temp = x[j-1];
+                    x[j-1] = x[j];
+                    x[j] = temp;
+                }else break;
+            }
+        }
+        System.out.println(Arrays.toString(x));
+    }
 }
