cef4f0e8cd66e0a59c97fa40077e6d759b9679ce
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-07 23:19:06 +0530

    cyclis sort

--- things-before-concept/Arrays/sorting/src/CyclicSort.java
+++ things-before-concept/Arrays/sorting/src/CyclicSort.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class CyclicSort {
+    public static void main(String[] args) {
+        int[] x = {2,1,5,4,3};
+        int[] y = {};
+        cyclicSort(x);
+    }
+    static void cyclicSort(int[] x){
+        int curelem = 1;
+        int ix = 0;
+        while (ix < x.length){
+            if(ix+1 != x[ix]){
+                int temp = x[ix];
+                x[ix] = x[ix+1];
+                x[ix+1] = temp;
+            }
+            ix++;
+        }
+        System.out.println(Arrays.toString(x));
+    }
 }
