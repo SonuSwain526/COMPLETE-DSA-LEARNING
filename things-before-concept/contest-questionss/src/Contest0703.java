74254347543ae1fa9482d0f965e2ecb9a85d1a51
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-07 23:20:19 +0530

    contest by senior

--- things-before-concept/contest-questionss/src/Contest0703.java
+++ things-before-concept/contest-questionss/src/Contest0703.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Contest0703 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+        }
+        int z = 0;
+        int ix = 0;
+        while (ix<x.length-1){
+            if(x[ix] > x[ix+1]){
+                x[ix+1]++;
+                z++;
+            }else ix++;
+        }
+        System.out.println(z);
+    }
 }
