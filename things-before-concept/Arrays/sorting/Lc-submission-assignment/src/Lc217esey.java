b69d375ed51c16e286ce4b1a41df86c548230422
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-09 17:36:02 +0530

    contain duplecates low tsc

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc217esey.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc217esey.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc217esey {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
 }
