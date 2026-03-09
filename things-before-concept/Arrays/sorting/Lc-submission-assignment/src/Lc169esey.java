c6b6d33549c160f883d004b9ad1b1f860c2c00f5
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-09 17:33:31 +0530

    majority elem moors law

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc169esey.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc169esey.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Lc169esey {
+    public int majorityElement(int[] nums) {
+        // for(int i = 0; i<nums.length; i++){
+        //     int c = 0;
+        //     for(int j = 0; j<nums.length; j++){
+        //         if(nums[i] == nums[j]){
+        //             c++;
+        //         }
+        //     }
+        //     if(c > nums.length/2){
+
+        //         return nums[i];
+        //     }
+        // }
+        // return 0;
+
+
+
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(x == 0){
+                y = nums[i];
+                x++;
+            }else if(y == nums[i]){
+                x++;
+            }else x--;
+        }
+        return y;
+    }
 }
