0ea62d62a9b66047cb4a4d3ba39d036e57f0a1e3
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-10 18:13:45 +0530

    missing no cyclis sort

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc268esey.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc268esey.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc268esey {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            if((i) != nums[i]){
+                return (i);
+            }
+        }
+        return (nums.length);
+    }
 }
