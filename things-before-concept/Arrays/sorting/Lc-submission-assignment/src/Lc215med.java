b75928b50e5fc4bf569912574d235b4af104c781
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-10 18:12:33 +0530

    find kth largest elem (recursion)

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc215med.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc215med.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc215med {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
 }
