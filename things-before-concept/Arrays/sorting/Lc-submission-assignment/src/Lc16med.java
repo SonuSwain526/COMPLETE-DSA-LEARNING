e31710a2f9771a6bbf4e1efc29e6ba0137a35a63
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-10 18:08:45 +0530

    closest sum 3 no

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc16med.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc16med.java
@@ -4,40 +4,26 @@ public class Lc16med {
 
     public static void main(String[] args) {
         int[] nums = {-1,0,1,1,1,1,2,-1,-3};
-        System.out.println(threeSum(nums));
+//        System.out.println(threeSum(nums));
     }
 
-    public static List<List<Integer>> threeSum(int[] n) {
-        Arrays.sort(n);
-        List<List<Integer>> x = new ArrayList<>();
-
-        for (int i = 0; i < n.length; i++) {
-
-            if (i > 0 && n[i] == n[i - 1]) continue;
-
-            for (int j = i + 1; j < n.length; j++) {
-                if (j > i + 1 && n[j] == n[j - 1]) continue;
-
-                int curEl = -(n[i] + n[j]);
-                bs(x, n, j + 1, n.length - 1, curEl, n[i], n[j]);
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i<nums.length - 2; i++){
+            int s = i + 1, e = nums.length - 1;
+            while(s < e){
+                int sum = nums[i] + nums[s] + nums[e];
+                if(Math.abs(target - sum) < Math.abs(target - result)){
+                    result = sum;
+                }
+                if(sum == target){
+                    return result;
+                }else if(sum > target){
+                    e--;
+                }else s++;
             }
         }
-
-        return x;
-    }
-
-    public static void bs(List<List<Integer>> x, int[] y, int s, int e, int t, int a, int b) {
-
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-
-            if (y[mid] == t) {
-                x.add(Arrays.asList(a, b, y[mid]));
-                return;
-            }
-
-            if (y[mid] > t) e = mid - 1;
-            else s = mid + 1;
-        }
+        return result;
     }
 }
