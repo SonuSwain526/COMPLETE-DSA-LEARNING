0e6f4a6e46fc24267ad8b0ad8a678b5cce860762
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-10 18:14:56 +0530

    find single duplecate

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc287med.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc287med.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Lc287med {
+    public int findDuplicate(int[] nums) {
+        // Arrays.sort(nums);
+        //     for(int j = 0; j < nums.length-1; j++){
+        //         if(nums[j] == nums[j+1]){
+        //             return nums[j];
+        //         }
+        //     }
+        for(int x: nums){
+            int ix = Math.abs(x);
+            if(nums[ix]<0){
+                return ix;
+            }nums[ix] = -nums[ix];
+        }
+        return 0;
+    }
 }
