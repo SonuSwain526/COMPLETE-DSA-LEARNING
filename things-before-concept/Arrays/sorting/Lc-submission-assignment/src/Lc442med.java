ddd1ee89931fa40096dcf82707eef9dc8bb261ee
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-11 18:58:09 +0530

    find duplecate no

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc442med.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc442med.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lc442med {
+
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+        int[] newArr = new int[n + 1];
+        for (int i : nums) {
+            newArr[i]++;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < newArr.length; i++) {
+            if (newArr[i] >= 2) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
 }
