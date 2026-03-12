68d0e70d7e20299982837c6efa8a5a561c2496f5
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-12 21:44:02 +0530

    esey one

--- things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc628esey.java
+++ things-before-concept/Arrays/sorting/Lc-submission-assignment/src/Lc628esey.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Lc628esey {
+    public static void main(String[] args) {
+
+        int[] nums = {-5,-4,1,5};
+
+        int x =Integer.MIN_VALUE;
+        int y = Integer.MIN_VALUE;
+        int c = Integer.MIN_VALUE;;
+
+        for(int n: nums){
+            if(x > n || y > n || c > n){
+                if(x > n){ x = n;};
+            }
+            else if(x > n || y > n || c > n){
+                if(y > n){ y = n;};
+            }
+            else if(x > n || y > n || c > n){
+                if(c > n){ c = n;};
+            }
+        }
+
+    }
 }
