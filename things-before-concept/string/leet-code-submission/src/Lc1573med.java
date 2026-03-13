37f7f9c88977ade56e6fa6b20209af6f73e666ed
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-13 20:38:15 +0530

    no of ways of sub stiring accroding to no of 1s

--- things-before-concept/string/leet-code-submission/src/Lc1573med.java
+++ things-before-concept/string/leet-code-submission/src/Lc1573med.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Lc1573med {
+    public static void main(String[] args) {
+        System.out.println(numWays("10101"));
+    }
+    public static int numWays(String s) {
+        int n = s.length();
+        int i = 1;
+        int j = n - 1;
+        int c = 0;
+
+        while (i < j) {
+
+            if (countone(s.substring(0, i)) == countone(s.substring(i, j)) &&
+                    countone(s.substring(i, j)) == countone(s.substring(j, n))) {
+
+                c++;
+            }
+
+            j--;
+
+            if (j <= i) {
+                i++;
+                j = n - 1;
+            }
+        }
+    return c;
+    }
+    public static int countone(String s){
+        int count = 0;
+        for(char c : s.toCharArray()){
+            if(c == '1') count++;
+        }
+        return count;
+    }
 }
