f23fbb1142c32d6eddd1c35f30d434873b62a9bc
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-13 20:35:38 +0530

    converting ip add to structured format

--- things-before-concept/string/leet-code-submission/src/Lc1108esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1108esey.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Lc1108esey {
+    public static void main(String[] args) {
+        System.out.println(defangIPaddr("23.es.sf"));
+    }
+    public static String defangIPaddr(String address) {
+        StringBuilder x = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) {
+            if(address.charAt(i) == '.'){
+                x.append("[.]");
+            }else x.append(address.charAt(i));
+        }
+       return x.toString();
+    }
 }
