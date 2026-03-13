885e50ec7ec6c3fc90c80f2cd8d9d42841d61c60
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-13 19:04:02 +0530

    contest questions string

--- things-before-concept/contest-questionss/src/Context0703A.java
+++ things-before-concept/contest-questionss/src/Context0703A.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Context0703A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count =1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c >= 'A' && c <= 'Z') {
+                count++;
+                System.out.println(c + " uppercase");
+            }
+        }
+        System.out.println(count);
+    }
 }
