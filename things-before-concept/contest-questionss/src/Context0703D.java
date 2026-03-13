b02263c994625da8f8b22586f530fbf03381dde7
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-13 20:34:13 +0530

    context ques arr

--- things-before-concept/contest-questionss/src/Context0703D.java
+++ things-before-concept/contest-questionss/src/Context0703D.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Context0703D {
+    public static void main(String[] args) {
+        int x = 5;
+        int as = (int) Math.pow(2,x-1);
+        System.out.println(as);
+    }
 }
