00648bd992b97b5535ca39d8bf6abac010ac9572
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-13 20:36:39 +0530

    sorting strig arr to ix index

--- things-before-concept/string/leet-code-submission/src/Lc1528esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1528esey.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc1528esey {
+    public static void main(String[] args) {
+        String x = "codeleet";
+        int[] y = {4,5,6,7,0,2,1,3};
+        System.out.println(restoreString(x, y));
+
+    }
+    public static String restoreString(String s, int[] indices) {
+        StringBuilder x = new StringBuilder();
+        char[] ans = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            ans[indices[i]] = s.charAt(i);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            x.append(ans[i]);
+        }
+
+        return x.toString();
+    }
 }
