0f0621d39240f2de9bf46ff4aedf9f18e175ba4a
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-14 17:18:24 +0530

    interpritin bsically replaceing proper sequence esey

--- things-before-concept/string/leet-code-submission/src/Lc1638esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1638esey.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Lc1638esey {
+    public static void main(String[] args) {
+        System.out.println(interpret("(al)G()()"));
+    }
+    public static String interpret(String c) {
+        int ix = 0;
+        StringBuilder x = new StringBuilder();
+        while(ix<c.length()){
+            if(c.charAt(ix) == '('){
+                if(c.charAt((ix+1)) == 'a'){
+                    x.append("al");
+                    ix = ix+4;
+                }else {
+                    x.append("0");
+                    ix = ix+2;
+                }
+            }else {
+                x.append("G");
+                ix++;
+            }
+        }
+        return x.toString();
+    }
 }
