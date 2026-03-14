6ba59770f9bfc9b6263bf3b6a27e6b1cb3467bf8
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-14 17:23:02 +0530

    max substring matching of 2 str

--- things-before-concept/string/leet-code-submission/src/Lc1668esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1668esey.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Lc1668esey {
+    public static void main(String[] args) {
+        System.out.println(maxRepeating("ababc", "ab"));
+    }
+    public static int maxRepeating(String s, String w) {
+        int wL = w.length();
+        int sL = s.length();
+        int c = 0;
+        int ix = 0;
+        while (ix < sL - wL){
+            if(s.substring(ix, ix+wL).equals(w)){
+                c++;
+            }
+            ix = ix+wL;
+        }
+        return c;
+    }
 }
