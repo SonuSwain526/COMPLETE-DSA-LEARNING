db07c509642b8fd36786bdc792a3dbb8b0c1a61a
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-14 17:16:58 +0530

    checking is pallindrom for prifix and suffix of string

--- things-before-concept/string/leet-code-submission/src/Lc1616med.java
+++ things-before-concept/string/leet-code-submission/src/Lc1616med.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Lc1616med {
+    public static void main(String[] args) {
+//        System.out.println(c("ulacfd", "jizalu"));
+    }
+
+    boolean check(String a, String b) {
+        int l = 0;
+        int r = a.length() - 1;
+
+        while (l < r && a.charAt(l) == b.charAt(r)) {
+            l++;
+            r--;
+        }
+
+        return isPalindrome(a, l, r) || isPalindrome(b, l, r);
+    }
+
+    boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--))
+                return false;
+        }
+        return true;
+    }
 }
