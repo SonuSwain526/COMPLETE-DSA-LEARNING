ed93824a3846a4135df7c0b38ce92d6aad3b91dc
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-14 17:22:02 +0530

    arr of string comparing esey

--- things-before-concept/string/leet-code-submission/src/Lc1662esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1662esey.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Lc1662esey {
+    public static void main(String[] args) {
+
+    }
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder x = new StringBuilder();
+        StringBuilder y = new StringBuilder();
+        for (int i = 0; i < word1.length; i++) {
+            x.append(word1[i]);
+            y.append(word2[i]);
+        }
+        return x.compareTo(y) == 0;
+    }
 }
