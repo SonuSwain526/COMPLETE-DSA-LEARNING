6ee9f07982e7e765a15ff9c19619263f3463be61
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-15 15:52:08 +0530

    College contest dt 14mar arr

--- things-before-concept/contest-questionss/dt-15-mar/src/Q2.java
+++ things-before-concept/contest-questionss/dt-15-mar/src/Q2.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Q2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+        }
+
+        Arrays.sort(x);
+
+        long sum = 0;
+        int j = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            sum += x[i] * (1L << j);
+            j++;
+        }
+
+        System.out.println(sum);
+    }
 }
