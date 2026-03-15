26aca9cfceefe221de6491bb69ed490f1d682ec9
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-15 15:58:57 +0530

    minimum length can possible by deleting char

--- things-before-concept/string/leet-code-submission/src/Lc1750med.java
+++ things-before-concept/string/leet-code-submission/src/Lc1750med.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Lc1750med {
+    public static void main(String[] args) {
+        System.out.println(minimumLength("aaaaaaaaaaaaa"));
+    }
+    public static int minimumLength(String s) {
+        int i = 0, j= s.length()-1;
+
+        while (i < j){
+            if(s.charAt(i) == s.charAt(j)){
+                if(s.charAt(i) == s.charAt(i+1) && s.charAt(j) != s.charAt(j-1)){
+                    i++;
+                } else if (s.charAt(j) == s.charAt(j-1) && s.charAt(i) != s.charAt(i+1)) {
+                    j--;
+                }else if (s.charAt(j) == s.charAt(j-1) && s.charAt(j) == s.charAt(i)) {
+                    j--;
+                    i++;
+                }else if (s.charAt(j) != s.charAt(j-1) && s.charAt(i) != s.charAt(i+1)) {
+                    j--;
+                    i++;
+                }
+            }else return j - i + 1;
+        }
+        return j - i + 1;
+    }
 }
