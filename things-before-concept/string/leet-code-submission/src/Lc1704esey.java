cba69bf142ef0aa601b163674ac594c8a176bf4f
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-15 15:58:00 +0530

    checking no of voulses same or not in 2 str

--- things-before-concept/string/leet-code-submission/src/Lc1704esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1704esey.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Lc1704esey {
+    public static void main(String[] args) {
+        halvesAreAlike("heeloo");
+    }
+    public static void halvesAreAlike(String s) {
+        int c = 0, d = 0;
+        int n = s.length();
+        for (int i = 0; i < n/2; i++) {
+            if("aeiouAEIOU".indexOf(s.charAt(i)) != -1){
+                c++;
+            }
+            if("aeiouAEIOU".indexOf(s.charAt(i + n/2)) != -1){
+                d++;
+            }
+        }
+        System.out.println(c);
+        System.out.println(d);
+    }
 }
