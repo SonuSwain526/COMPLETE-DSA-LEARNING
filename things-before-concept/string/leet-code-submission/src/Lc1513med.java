f1498d42735b8e1f563f39619173d6604002e179
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-15 15:55:16 +0530

    sub arr of num sequence binary string

--- things-before-concept/string/leet-code-submission/src/Lc1513med.java
+++ things-before-concept/string/leet-code-submission/src/Lc1513med.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Lc1513med {
+    public static void main(String[] args) {
+        System.out.println(numSub("1111"));
+    }
+    public static int numSub(String s) {
+        int c = 0;
+        int sum = 0;
+        int mod = 1000000007;
+        for(char x: s.toCharArray()){
+            if(x == '1'){
+                c++;
+                sum += c;
+            }else c = 0;
+            sum = sum % mod;
+        }
+        return sum;
+    }
+
 }
