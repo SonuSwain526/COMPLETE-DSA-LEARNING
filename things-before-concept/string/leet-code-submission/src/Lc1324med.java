00c6d91ed2247736a0974b197bd846922bc8f99e
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-16 11:38:01 +0530

    printing vertically of word of a string approch 2d arr

--- things-before-concept/string/leet-code-submission/src/Lc1324med.java
+++ things-before-concept/string/leet-code-submission/src/Lc1324med.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lc1324med {
+    public static void main(String[] args) {
+        System.out.println(printVertically("HOW ARE YOU"));
+    }
+    public static List<String> printVertically(String s) {
+        List<String> x = new ArrayList<>();
+        boolean ex = true;
+        int i = 0;
+        int ix = 0;
+        while (ix < s.length()){
+            if(s.charAt(ix) != ' '){
+                if(i < x.size() || x.isEmpty()|| ex) {
+                    if (ex) x.add(i, String.valueOf(s.charAt(ix)));
+                    else x.set(i, x.get(i) + s.charAt(ix));
+                    i++;
+                }
+                else {
+                    x.add(i, String.valueOf(s.charAt(ix)));
+                }
+
+            }else {i = 0; ex = false;}
+            ix++;
+        }
+        return x;
+    }
 }
