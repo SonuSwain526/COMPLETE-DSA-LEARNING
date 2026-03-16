26a8f5cdc96f04c5e713149d4155a11cfe022182
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-16 11:41:01 +0530

    counting homogenehous char in string parttern

--- things-before-concept/string/leet-code-submission/src/Lc1759med.java
+++ things-before-concept/string/leet-code-submission/src/Lc1759med.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Lc1759med {
+    public static void main(String[] args) {
+        System.out.println(countHomogenous("a"));
+    }
+    public static int countHomogenous(String s) {
+        int i = 0;
+        int j = 0;
+
+        int sum = 0;
+        int c = 0;
+
+        while (i < s.length() && j < s.length()){
+            if(s.charAt(i) == s.charAt(j)){
+                c++;
+                sum += c;
+                j++;
+            }else{
+                i = j;
+                c = 0;
+            }
+        }
+        return sum;
+    }
 }
