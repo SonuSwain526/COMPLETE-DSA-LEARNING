f7a1259d71e1be9ca3537eccc4ab7317d5fceba6
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-16 11:42:18 +0530

    finding spesific parttern in 2d arr type clr ruleval

--- things-before-concept/string/leet-code-submission/src/Lc1773esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1773esey.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Objects;
 
 public class Lc1773esey {
+    public static void main(String[] args) {
+
+    }
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int x;
+        if(ruleKey.equals("type")){
+            x = 0;
+        } else if (ruleKey.equals("color")) {
+            x = 1;
+        }else x = 2;
+        int c = 0;
+        for (int i = 0; i < items.size(); i++) {
+            if(Objects.equals(items.get(i).get(x), ruleValue)){
+                c++;
+            }
+        }
+        return c;
+    }
 }
