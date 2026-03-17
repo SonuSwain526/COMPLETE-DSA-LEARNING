3fcf1591a7b48efdfd15b7a85aa37691eeb97a09
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-17 23:05:02 +0530

    checking segement esey

--- things-before-concept/string/leet-code-submission/src/Lc1784esey.java
+++ things-before-concept/string/leet-code-submission/src/Lc1784esey.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Lc1784esey {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean checkOnesSegment(String s) {
+        for (int i = 0; i < s.length()-1; i++) {
+            if(s.charAt(i) == '0' && s.charAt(i+1) == '1'){
+                return true;
+            }
+        }
+        return false;
+    }
 }
