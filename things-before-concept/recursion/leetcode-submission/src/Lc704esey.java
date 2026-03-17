2fe7065de0163e54c5b0e4be87870383a5e22fb1
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-17 23:04:08 +0530

    binary search using recursion

--- things-before-concept/recursion/leetcode-submission/src/Lc704esey.java
+++ things-before-concept/recursion/leetcode-submission/src/Lc704esey.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Lc704esey {
+//    public static void main(String[] args) {
+//        System.out.println(x(nums, 0, nums.length-1, target));
+//    }
+//    public static int x(int[] a, int l, int r, int t){
+//        int mid = l + (r-l)/2;
+//        if(l > r) return -1;
+//        if(a[mid] == t){
+//            return mid;
+//        }else if(a[mid] > t){
+//            return x(a, 0, mid-1, t);
+//        }else return x(a, mid+1, a.length-1, t);
+//    }
 }
