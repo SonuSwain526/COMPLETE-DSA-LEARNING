46c637f54754c495bec6d6f0dadf974ea20c8ce7
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-18 23:35:38 +0530

    is number checking by applying pure rules

--- things-before-concept/string/leet-code-submission/src/Lc65hard.java
+++ things-before-concept/string/leet-code-submission/src/Lc65hard.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Lc65hard {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isNumber(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) && (c != 'e' && c != 'E')){
+                return false;
+            }else if(c == '+' || c == '-'){
+                if(s.charAt(i+1) == '+' || s.charAt(i+1) == '-'){
+                    return false;
+                }
+            } else if (c == '.' && s.charAt(i+1) == '.') {
+                return false;
+            } else if (c == 'e') {
+                if(Checkep(s.substring(i+1))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean Checkep(String s){
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '.'){
+                return true;
+            }
+        }
+        return false;
+    }
 }
