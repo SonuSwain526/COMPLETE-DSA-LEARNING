1fa18aab09b4cfdd15c0a7af5cec5b409f9ba187
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-21 11:04:59 +0530

    unsolved

---
+++ things-before-concept/recursion/leetcode-submission/src/Lc39med.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Lc39med {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> x = Collections.singletonList(new ArrayList<Integer>());
+        return x;
+    }
+}
