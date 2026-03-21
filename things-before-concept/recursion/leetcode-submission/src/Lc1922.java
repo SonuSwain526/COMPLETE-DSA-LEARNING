2fc3b925b71d1b25dd04204d65ba5ae61f6db37f
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-21 23:49:26 +0530

    counting good number

--- things-before-concept/recursion/leetcode-submission/src/Lc1922.java
+++ things-before-concept/recursion/leetcode-submission/src/Lc1922.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Lc1922 {
+    public static void main(String[] args) {
+        System.out.println(countGoodNumbers(30));
+    }
+    public static int countGoodNumbers(long n) {
+        return Math.toIntExact(x(n));
+    }
+    public static long x(long n){
+        int x = 1000000007;
+        if(n == 1) { return 5;}
+        x(n-1);
+        if(n % 2 == 0) return (x(n-1) * 4L) % x;
+        else return (x(n-1) * 5L) % x;
+    }
 }
