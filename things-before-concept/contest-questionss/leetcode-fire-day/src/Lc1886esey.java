fab242cee44f22b12f8cca84fc0c205d6c0afdee
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-23 23:41:05 +0530

    lc fire day 3

--- things-before-concept/contest-questionss/leetcode-fire-day/src/Lc1886esey.java
+++ things-before-concept/contest-questionss/leetcode-fire-day/src/Lc1886esey.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc1886esey {
+    public static void main(String[] args) {
+
+    }
+    public static boolean findRotation(int[][] mat, int[][] target) {
+        return findRotation(mat, target);
+    }
+    public boolean findRotation(int[][] mat, int[][] target, int x) {
+        if(x == 4)return false;
+        int[][] ra = new int[mat.length][mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                ra[i][j] = mat[mat.length - j - 1][i];
+            }
+        }
+        if(Arrays.deepEquals(ra, target)){
+            return true;
+        }
+        return findRotation(ra, target, x+1);
+    }
 }
