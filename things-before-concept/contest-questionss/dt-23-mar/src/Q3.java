75827a0b1a5cbb20c91a8d33073f6ef4b2a8b6bb
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-23 23:40:29 +0530

    contest 23 mar

--- things-before-concept/contest-questionss/dt-23-mar/src/Q3.java
+++ things-before-concept/contest-questionss/dt-23-mar/src/Q3.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int pl = -1;
+        for (int i = 0; i < sc.nextInt(); i++) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            if(Math.abs((a - b)) > max){
+                max = Math.abs((a - b));
+                if(a > b){
+                    pl = 1;
+                }else pl = 2;
+            }
+        }
+        System.out.println(max + "," + pl);
+    }
 }
