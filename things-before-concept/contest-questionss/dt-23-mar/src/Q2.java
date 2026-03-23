30db0a5278a11c399340de095d5658fb07103343
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-23 23:39:59 +0530

    contest mar 23

--- things-before-concept/contest-questionss/dt-23-mar/src/Q2.java
+++ things-before-concept/contest-questionss/dt-23-mar/src/Q2.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+//        int[] a = new int[3];
+//        int[] b = new int[3];
+        int x = 0, y=0;
+        for (int i = 0; i < 3; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a > b) x++;
+            if(b > a) y++;
+        }
+
+        System.out.println(Arrays.toString(new int[]{x, y}));
+    }
 }
