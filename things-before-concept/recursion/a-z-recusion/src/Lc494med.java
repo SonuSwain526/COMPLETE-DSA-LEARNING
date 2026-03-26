d6fd39f1ef6a39b234355f22d8ccd7c8ed1c6b2c
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-26 23:25:01 +0530

    target sum, given arr try different combination for a ans only +,-

--- things-before-concept/recursion/a-z-recusion/src/Lc494med.java
+++ things-before-concept/recursion/a-z-recusion/src/Lc494med.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Lc494med {
+    public static void main(String[] args) {
+        System.out.println(x(new int[]{1,1,1,1,1}, 3, 0, 0,0));
+    }
+    public static int x(int[] nums, int target, int sum, int ix, int c) {
+        if(sum == target && ix == nums.length){
+            return c+1;
+        } else if (ix >= nums.length) {
+            return c;
+        }
+
+        int count = c;
+        count = x(nums, target, sum + nums[ix], ix+1, count);
+        count = x(nums, target, sum - nums[ix], ix+1, count);
+        return count;
+    }
 }
