6db39ff3d17ba8a494bdded573d710ef08775be8
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-26 23:26:35 +0530

    power set

--- things-before-concept/recursion/a-z-recusion/src/PowerSet.java
+++ things-before-concept/recursion/a-z-recusion/src/PowerSet.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PowerSet {
+    public static void main(String[] args) {
+        List<List<Integer>> l = new ArrayList<>();
+        int[] nums = {1,2,3};
+        ArrayList<Integer> temp = new ArrayList<>();
+        x(l, 0, temp, nums);
+        System.out.println(l);
+    }
+    public static void x(List<List<Integer>> l, int i, ArrayList<Integer> temp, int[] nums){
+        if(i == nums.length){ l.add(new ArrayList<>(temp));return;}
+
+//        l.add(new ArrayList<>(temp));
+        if(i< nums.length) temp.add(nums[i]);
+        x(l, i+1, temp, nums);
+        temp.removeLast();
+        x(l, i+1, temp, nums);
+    }
 }
