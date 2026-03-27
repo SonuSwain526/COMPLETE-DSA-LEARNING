88a1d0ac1eb87baff68744603f4ff7163d496d1c
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-27 18:20:35 +0530

    fire day 7 matrix esey qes

---
+++ things-before-concept/contest-questionss/dt-25-mar/src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//
+        Scanner sc = new Scanner(System.in);
+////        int T = sc.nextInt();
+////        for (int i = 0; i < T; i++) {
+////            int A = sc.nextInt();
+////            int B = sc.nextInt();
+////            if(A > B){
+////                System.out.println(">");
+////            } else if (A<B) {
+////                System.out.println("<");
+////            }else System.out.println("=");
+//
+//        int n = sc.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            String word = sc.next();
+//            if(word.length() < 10) {
+//                System.out.println(word);
+//            }else{
+//                String x = "" + word.charAt(0) + (word.length() - 2) + word.charAt(word.length() - 1);
+//                System.out.println(x);
+//            }
+//        }
+
+//
+//        int n = sc.nextInt();
+//        int x = sc.nextInt();
+//        int[] ar = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            ar[i] = sc.nextInt();
+//        }
+//
+//        Arrays.sort(ar);
+//        int l = 0, r = n-1;
+//        while (l <= r){
+//            if(ar[l] + ar[r] >= x){
+//                r--;
+//            }else {
+//                int req = x - (ar[l] + ar[r]);
+//                int ix = l+1;
+//                while (ix < r) {
+//                    if (req == ar[ix]) {
+//                        System.out.println(ar[l] + " " + ar[ix] + " " + ar[r]);
+//                        return;
+//                    }
+//                    ix++;
+//                }
+//            }
+//        }
+//        System.out.println("IMPOSSIBLE");
+
+
+//
+//        int n = sc.nextInt();
+//        int[] a = new int[n];
+//        int c = 0;
+//        for (int i = 0; i < n; i++) {
+//            a[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < n; i++) {
+//            int sum = 0;
+//            for (int j = i; j < n; j++) {
+//                sum += a[j];
+//                if(sum % n ==0)c++;
+//            }
+//        }
+//        System.out.println(c);
+
+
+    }
+}
--- things-before-concept/contest-questionss/leetcode-fire-day/src/Lc2946esey.java
+++ things-before-concept/contest-questionss/leetcode-fire-day/src/Lc2946esey.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Lc2946esey {
+    public static void main(String[] args) {
+        int[][] x = {
+                {3,10,3,10,3,10,3,10},
+                {5,8,5,8,5,8,5,8},
+                {3,9,3,9,3,9,3,9},
+                {3,8,3,8,3,8,3,8},
+                {2,3,2,3,2,3,2,3}
+        };
+        System.out.println(areSimilar(x, 2));
+    }
+    public static boolean areSimilar(int[][] mat, int k) {
+        int[][] copy = new int[mat.length][mat[0].length];
+        int[][] copy1 = new int[mat.length][mat[0].length];
+         for(int i = 0; i < mat.length; i++) {
+            System.arraycopy(mat[i], 0, copy[i], 0, mat[0].length);
+        }
+        for(int i = 0; i < mat.length; i++) {
+            System.arraycopy(mat[i], 0, copy1[i], 0, mat[0].length);
+        }
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < mat.length; j = j+2) {
+                int temp = mat[j][0];
+                for (int l = 0; l < mat[0].length-1; l++) {
+                    mat[j][l] = copy[j][l+1];
+                }
+                mat[j][mat[0].length-1] = temp;
+            }
+
+            for (int j = 1; j < mat.length; j = j+2) {
+                int temp = mat[j][mat[0].length-1];
+                for (int l = 0; l < mat[0].length-1; l++) {
+                    mat[j][l+1] = copy[j][l];
+                }
+                mat[j][0] = temp;
+            }
+            for(int m = 0; m < mat.length; m++) {
+                System.arraycopy(mat[m], 0, copy[m], 0, mat[0].length);
+            }
+        }
+        for(int[] x: mat){
+            System.out.println(Arrays.toString(x));
+        }
+        return Arrays.deepEquals(mat, copy1);
+
+    }
 }
