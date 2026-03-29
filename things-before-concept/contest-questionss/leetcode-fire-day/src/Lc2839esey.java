33f1b6f9b929a3dee33617030e3688bf68ec72e2
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-03-29 22:40:42 +0530

    checking str ewqulisation

---
+++ things-before-concept/contest-questionss/leetcode-fire-day/src/Lc2839esey.java
@@ -0,0 +1,13 @@
+public class Lc2839esey {
+    public static void main(String[] args) {
+
+    }
+    public static boolean canBeEqual(String s1, String s2) {
+        if(s1.equals(s2)) return true;
+        else {
+            return ((s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0) && s1.charAt(1) == s2.charAt(3)) && s1.charAt(3) == s2.charAt(1))
+                    || ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2) && s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1))
+                    || (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3) && s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0)));
+        }
+    }
+}
