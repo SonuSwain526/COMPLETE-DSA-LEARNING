bbe83a1924cae274bf3e270a78a32fde7e118f69
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-04-11 23:32:42 +0530

    contest qeus 10 april

---
+++ things-before-concept/contest-questionss/dt-10-apr/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+////        int n = sc.nextInt();
+////        for (int i = 0; i < n; i++) {
+////            int X = sc.nextInt();
+////            int Y = sc.nextInt();
+////            System.out.println(Math.abs(X- Y));
+////        }
+//
+////        int t = sc.nextInt();
+////        int n = sc.nextInt();
+////        for (int i = 0; i < ; i++) {
+////
+////        }
+//        System.out.println(angryProfessor(3, new int[]{-1,-3,4,2}));
+//    }
+    public static String angryProfessor(int k, List<Integer> a){
+        int c = 0;
+//        for (int i = 0; i < a.length; i++) {
+////            if(a[i] >= 0){
+////                c++;
+////            }
+//            if(c >= k) return "NO";
+//        }
+        return "YES";
+    }
+}
