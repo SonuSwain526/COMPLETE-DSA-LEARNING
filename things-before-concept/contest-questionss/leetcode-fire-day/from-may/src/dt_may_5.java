08bead89b3b60d61f4c31cf5b3ad429a0747babe
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-05-05 23:20:05 +0530

    fire day

--- things-before-concept/contest-questionss/leetcode-fire-day/from-may/src/dt_may_5.java
+++ things-before-concept/contest-questionss/leetcode-fire-day/from-may/src/dt_may_5.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class dt_may_5 {
+    public static void main(String[] args) {
+        int[][] m = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        rotate(m);
+    }
+    public static void rotate(int[][] matrix) {
+        int l = 0, r = matrix.length-1, t = 0, d = matrix.length-1;
+        int [][]x = new int[matrix.length][matrix.length];
+        while (l <= r && t <= d){
+            int a = r;
+            for (int i = l; i <= r; i++) {
+//                x[t][i] = matrix[i][l];
+//                x[t][i] = matrix[l][a];
+                x[i][r] = matrix[t][i];
+                a--;
+            }
+            int j = d;
+            for (int i = t; i <= d; i++) {
+                x[t][i] = matrix[j][l];
+                x[d][j] = matrix[i][r];
+                x[l][j] = matrix[d][j];
+                j--;
+            }
+            l++;
+            r--;
+            t++;
+            d--;
+        }
+
+        for(int[] xx: x){
+            System.out.println(Arrays.toString(xx));
+        }
+    }
 }
