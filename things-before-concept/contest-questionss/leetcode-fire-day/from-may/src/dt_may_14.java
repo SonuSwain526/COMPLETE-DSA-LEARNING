c7e264841fa54fb7b685c48a483d73e02b4a18ad
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-05-14 23:59:26 +0530

    fire day

--- things-before-concept/contest-questionss/leetcode-fire-day/from-may/src/dt_may_14.java
+++ things-before-concept/contest-questionss/leetcode-fire-day/from-may/src/dt_may_14.java
@@ -1,2 +1,46 @@
 public class dt_may_14 {
+    public static void main(String[] args) {
+
+    }
+      public class ListNode {
+          int val;
+         ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+
+        ListNode n = new ListNode(-1);
+        while (t1 != null){
+            ListNode x = n.next;
+            ListNode newn = new ListNode(t1.val);
+            n.next = newn;
+            newn.next = x;
+            t1 = t1.next;
+        }
+
+        ListNode n1 = new ListNode(-1);
+        while (t2 != null){
+            ListNode x = n1.next;
+            ListNode newn = new ListNode(t2.val);
+            n1.next = newn;
+            newn.next = x;
+            t2 = t2.next;
+        }
+
+        t1 = n.next;
+        t2 = n1.next;
+        while (t1 != null && t2 != null){
+            if(t1.next.val != t2.next.val){
+                return t1;
+            }
+        }
+
+//        while()
+        return t1;
+    }
 }
