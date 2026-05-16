f0b43fd9443e0c0ec7ae216951b2b0da7f537c15
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-05-16 10:06:44 +0530

    linked list

--- things-before-concept/linked-list/basic-things/src/Pring_ll.java
+++ things-before-concept/linked-list/basic-things/src/Pring_ll.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Pring_ll {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node a = new Node(20);
+        Node b = new Node(30);
+        Node c = new Node(40);
+
+        head.next = a;
+        a.next = b;
+        b.next = c;
+
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
 }
