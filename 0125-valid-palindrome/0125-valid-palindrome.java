282d631730e73f4bad36a19e05df4113dc33b92f
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-06-21 23:01:21 +0530

    Time: 2 ms (99.22%), Space: 44.4 MB (57.4%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String q) {
+        String s = q.toLowerCase();
+        if(s.length() == 1) return true;
+        int i = 0; 
+        int j = s.length()-1;
+
+        while(i < j){
+           if((s.charAt(i) < 'a' || s.charAt(i) > 'z') &&
+   (s.charAt(i) < '0' || s.charAt(i) > '9')){
+    i++;
+    continue;
+}
+
+if((s.charAt(j) < 'a' || s.charAt(j) > 'z') &&
+   (s.charAt(j) < '0' || s.charAt(j) > '9')){
+    j--;
+    continue;
+}
+
+            if(s.charAt(i) != s.charAt(j)) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
