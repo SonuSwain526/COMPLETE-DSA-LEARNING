930133465215643972a9d512a8e065d980b56450
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-07-12 18:09:11 +0530

    Time: 1 ms (18.98%), Space: 44.2 MB (16.77%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        List<TreeNode> list = new ArrayList<>();
+        helper(root, list);
+        // System.out.println(l);
+        for (int i = 0; i < list.size() - 1; i++) {
+            list.get(i).left = null;
+            list.get(i).right = list.get(i + 1);
+        }
+        list.get(list.size() - 1).left = null;
+        list.get(list.size() - 1).right = null;
+
+        // root = x;
+    }
+
+    TreeNode copy(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode node = new TreeNode(root.val);
+        node.left = copy(root.left);
+        node.right = copy(root.right);
+
+        return node;
+    }
+
+    void helper(TreeNode cur, List<TreeNode> l){
+        if (cur == null) return;
+
+        l.add(cur);
+        helper(cur.left, l);
+        helper(cur.right, l);
+    }
+}
