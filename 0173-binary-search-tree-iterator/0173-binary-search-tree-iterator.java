d85d7150baecf7b87a5e8b6ce5c34cf74d76ba13
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-07-14 19:13:14 +0530

    Time: 17 ms (95.43%), Space: 50.4 MB (22.44%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -15,31 +15,58 @@
  */
 class BSTIterator {
 
-    List<Integer> x;
-    int i;
+    Stack<TreeNode> s;
 
     public BSTIterator(TreeNode root) {
-        x = new ArrayList<>();
-        i = 0;
-        iot(root);
+        s = new Stack<>();
+        put(root);
     }
-    public void iot(TreeNode cur){
-        if (cur == null) return;
 
-        iot(cur.left);
-        x.add(cur.val);
-        iot(cur.right);
+    void put(TreeNode cur){
+        if(cur == null) return;
+        s.push(cur);
+        put(cur.left);
     }
     
     public int next() {
-        return x.get(i++);
+        TreeNode x =s.pop();;
+        if(x.right != null) put(x.right);
+        // s.pop();
+        return x.val;
     }
     
     public boolean hasNext() {
-        return (i < x.size());
+        return !s.isEmpty();
     }
 }
 
+
+// class BSTIterator {
+
+//     List<Integer> x;
+//     int i;
+
+//     public BSTIterator(TreeNode root) {
+//         x = new ArrayList<>();
+//         i = 0;
+//         iot(root);
+//     }
+//     public void iot(TreeNode cur){
+//         if (cur == null) return;
+
+//         iot(cur.left);
+//         x.add(cur.val);
+//         iot(cur.right);
+//     }
+    
+//     public int next() {
+//         return x.get(i++);
+//     }
+    
+//     public boolean hasNext() {
+//         return (i < x.size());
+//     }
+// }
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator obj = new BSTIterator(root);
