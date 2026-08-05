adc5709fd9993407c52c7e1a3462a2fefc382305
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-08-05 17:11:43 +0530

    Time: 227 ms (50.89%), Space: 116.2 MB (25.57%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+
+        HashMap <Integer, Integer> freq = new HashMap<>();
+
+        for (int el: hand) freq.put (el, freq.getOrDefault(el, 0) + 1);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int el: freq.keySet()) pq.add (el);
+
+        while (!pq.isEmpty()) {
+            int cur = pq.peek();
+
+            for (int i = 0; i < groupSize; i++){
+
+                int next = cur + i;
+
+                if (!freq.containsKey(next)){
+                    return false;
+                }
+
+                freq.put (next, freq.get(next) - 1);
+
+                if (freq.get(next) == 0){
+
+                    if (pq.peek() != next) return false;
+
+                    pq.poll();
+                    freq.remove(next);
+                }
+            }
+        }
+
+        return true;
+    }
+}
