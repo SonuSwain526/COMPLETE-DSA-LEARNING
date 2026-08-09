073d220a821f143b88d6f9ca67a8fc75b526cf8f
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-08-09 19:03:31 +0530

    Time: 49 ms (27.17%), Space: 189.3 MB (5.82%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[][] highestPeak(int[][] mat) {
+
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[][] dist = new int[n][m];
+
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+
+        // Water cells are the sources
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (mat[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.add(new Pair<>(i, j));
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] directions = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        while (!q.isEmpty()) {
+
+            Pair<Integer, Integer> cur = q.poll();
+
+            int i = cur.getKey();
+            int j = cur.getValue();
+
+            for (int[] dir : directions) {
+
+                int ni = i + dir[0];
+                int nj = j + dir[1];
+
+                if (ni < 0 || ni >= n || nj < 0 || nj >= m)
+                    continue;
+
+                if (dist[ni][nj] > dist[i][j] + 1) {
+
+                    dist[ni][nj] = dist[i][j] + 1;
+
+                    q.add(new Pair<>(ni, nj));
+                }
+            }
+        }
+
+        return dist;
+    }
+}
