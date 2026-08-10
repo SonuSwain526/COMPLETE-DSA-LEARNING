eb83b07a7f4c3ae5c32345c6a56b0bf87c312f8a
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-08-10 18:15:40 +0530

    Time: 3 ms (26.15%), Space: 47.7 MB (75.97%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,23 +1,53 @@
 class Solution {
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        int n = board.length;      // rows
+        int m = board[0].length;   // columns
 
         boolean[][] vis = new boolean[n][m];
 
-        // List<Pair<Integer, Integer>> list = new ArrayList<>();
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
 
-        Queue<Pair <Integer, Integer>> q = new LinkedList<>();
-
+        // Left and right columns
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if ((i == 0 || i == n-1 || j == 0 || j == m - 1) && board[i][j] == 'O') {
-                    vis[i][j] = true;
-                    // list.add ( new Pair<>(i, j) );
-                    q.add (new Pair<> (i, j));
-                }
+
+            // Left column
+            if (board[i][0] == 'O') {
+                vis[i][0] = true;
+                q.add(new Pair<>(i, 0));
+            }
+
+            // Right column
+            if (board[i][m - 1] == 'O') {
+                vis[i][m - 1] = true;
+                q.add(new Pair<>(i, m - 1));
             }
         }
+
+        // Top and bottom rows
+        for (int j = 0; j < m; j++) {
+
+            // Top row
+            if (board[0][j] == 'O') {
+                vis[0][j] = true;
+                q.add(new Pair<>(0, j));
+            }
+
+            // Bottom row
+            if (board[n - 1][j] == 'O') {
+                vis[n - 1][j] = true;
+                q.add(new Pair<>(n - 1, j));
+            }
+        }
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         if ((i == 0 || i == n-1 || j == 0 || j == m - 1) && board[i][j] == 'O') {
+        //             vis[i][j] = true;
+        //             // list.add ( new Pair<>(i, j) );
+        //             q.add (new Pair<> (i, j));
+        //         }
+        //     }
+        // }
 
         while (!q.isEmpty()) {
             int size = q.size();
