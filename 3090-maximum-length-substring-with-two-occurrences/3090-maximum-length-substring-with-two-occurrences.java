f1043051138bd6afbf00142b7b1a8e468d82f255
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-08-14 16:19:00 +0530

    Time: 1 ms (100%), Space: 43.4 MB (88.11%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,21 +1,37 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
+        int n = s.length();
         int max = 0; 
 
-        for (int i = 0; i < s.length(); i++) {
-            int[] freq = new int[26];
-            freq[s.charAt(i) - 'a']++;
-            int cur = 1;
+        int[] freq = new int[26];
 
-            for (int j = i + 1; j < s.length(); j++) {
-                if (++freq[s.charAt(j) - 'a'] > 2) {
-                    max = Math.max(max, cur);
-                    break;
-                }else cur++;
-                max = Math.max(max, cur);
+        int st = 0; 
+        int en = 0; 
+
+        while (st < n && en < n ) {
+            freq[s.charAt(en) - 'a']++;
+            while (freq[s.charAt(en) - 'a'] > 2 && st < n) {
+                freq[s.charAt(st++) - 'a']--;
             }
-            max = Math.max(max, cur);
+            int size = en - st + 1;
+            en++;
+            max = Math.max(max, size);
         }
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     int[] freq = new int[26];
+        //     freq[s.charAt(i) - 'a']++;
+        //     int cur = 1;
+
+        //     for (int j = i + 1; j < s.length(); j++) {
+        //         if (++freq[s.charAt(j) - 'a'] > 2) {
+        //             max = Math.max(max, cur);
+        //             break;
+        //         }else cur++;
+        //         max = Math.max(max, cur);
+        //     }
+        //     max = Math.max(max, cur);
+        // }
 
         return max;
     }
