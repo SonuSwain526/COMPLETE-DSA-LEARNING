b2167aaadf7b6186215c777766a4d1794e1242f6
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-08-18 23:05:46 +0530

    Time: 13 ms (38.07%), Space: 49.6 MB (22.77%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<int[]>> adj = new ArrayList<>();
+
+        for (int i= 0; i <= n; i++) adj.add(new ArrayList<>());
+        
+        for (int[] x: times) {
+            adj.get(x[0]).add(new int[]{x[1], x[2]});
+        }
+        int[] dist = new int[n+1];
+        for (int i = 0; i <= n; i++) dist[i] = Integer.MAX_VALUE;
+        dist[k] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(k);
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+
+            for (int[] cur: adj.get(x)) {
+                int node = cur[0];
+                int distt = cur[1];
+                if (dist[x] + distt < dist[node]) {
+                    dist[node] = dist[x] + distt;
+                    q.add(node);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dist));
+        int ans = -1;
+        int c = 0;
+        for (int i: dist) {
+            if (c != 0 && i == Integer.MAX_VALUE) return -1;
+            if(i != Integer.MAX_VALUE) ans = Math.max(i, ans);
+            c++;
+        }
+
+        return ans;
+    }
+
+    // void topo(boolean[] vis, List<List<int[]>> adj, int src, Stack<Integer> st) {
+    //     vis[src] = true;
+
+    //     for (int[] cur: adj.get(src)) {
+    //         if (!vis[cur[0]]) {
+    //             topo(vis, adj, cur[0], st);
+    //         }
+    //     }
+    //     st.push(src);
+    // }
+}
