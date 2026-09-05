29d1c393dbe6bd30718d25628ed747cc706761c7
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-09-05 11:07:02 +0530

    Time: 1 ms (17.21%), Space: 43.4 MB (80.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,8 +1,19 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for (int[] x: dp) Arrays.fill(x, -1);
-        return helper(obstacleGrid, 0, 0, dp);
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) return 0; 
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - 1 >= 0 && obstacleGrid[i-1][j] != 1) dp[i][j] = dp[i-1][j];
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) dp[i][j] += dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+        // for (int[] x: dp) Arrays.fill(x, -1);
+        // return helper(obstacleGrid, 0, 0, dp);
     }
     int helper(int[][] x, int i, int j, int[][] dp) {
         if (i >= x.length || j >= x[0].length) return 0;
