86cd8865df4c90d11d336648106ad8d257f5e1f8
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-09-05 10:36:59 +0530

    Time: 0 ms (100%), Space: 42.9 MB (37.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
+        if (dp.length == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        return dph(nums, 0, dp);
+        for (int i = 2; i < nums.length; i++) {
+            int take = nums[i] + dp[i - 2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(take, skip);
+        }
+        // Arrays.fill(dp, -1);
+
+        // return dph(nums, 0, dp);
+        return dp[nums.length - 1];
     }
 
     int dph(int[] nums, int i, int[] dp) {
