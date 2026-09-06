ebc53531f7090a4d93c7676c8c03fe42cc3bc96c
Gourab Swain <167749331+SonuSwain526@users.noreply.github.com>
2026-09-06 10:44:50 +0530

    Time: 52 ms (44.85%), Space: 47.6 MB (55.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,10 +5,23 @@ class Solution {
 
         if (sum % 2 != 0) return false;
 
-        int[][] dp = new int[nums.length][sum / 2 + 1];
-        for (int[] x: dp) Arrays.fill(x, -1);
+        int t = sum / 2;
 
-        return f(nums, sum / 2, dp, 0);
+        boolean[][] dp = new boolean[nums.length][t + 1];
+
+        for (int i = 0; i < dp.length; i++) dp[i][0] = true;
+        if (nums[0] < t + 1) dp[0][nums[0]] = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 1; j < sum / 2 + 1; j++) {
+                boolean take = dp[i-1][j];
+                boolean skip = false;
+                if (nums[i] <= j) skip = dp[i - 1][j - nums[i]];
+                dp[i][j] = take || skip;
+            }
+        }
+
+        return dp[dp.length - 1][t];
     }
 
     boolean f(int[] nums, int tar, int[][] dp, int ix) {
